d70bb2b1811f4048f364a99320de3358898c3028
hxcva1 <hxcva1@gmail.com>
2025-03-04 14:52:02 +0900

    pub/sub 구독

--- src/main/java/com/example/HelloMessageQueue/step3/HomeController.java
+++ src/main/java/com/example/HelloMessageQueue/step3/HomeController.java
@@ -12,4 +12,10 @@ public class HomeController {
 		model.addAttribute("message", "Welcome to RabbitMQ Sample!");
 		return "home";
 	}
+
+	@GetMapping("/news")
+	public String news(Model model) {
+		model.addAttribute("message", "Welcome to RabbitMQ News Sample!");
+		return "news";
+	}
 }
